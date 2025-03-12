603adc35df5a6d8fed3cb44f7971810ac3d54c6e
manuyuuu <manuyuuu@icloud.com>
2025-03-12 21:17:28 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,28 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function(){
+window.onload = generateCard;
+
+document.getElementById("newCard").addEventListener("click", generateCard);
+
+function generateCard() {
   let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-  let shapes = ["♠","♥","♦","♣"];
+  let shapes = ["♠", "♥", "♦", "♣"];
   let randomNumber = Math.floor(Math.random() * numbers.length);
-  let randomShape = Math.floor(Math.random() * shapes.length);
+  let randomShape = Math.floor(Math.random() * shapes.length);  
   let number = numbers[randomNumber];
   let shape = shapes[randomShape];
 
+  document.querySelector(".number").innerText = number;
+  document.querySelector(".top").innerText = shape;
+  document.querySelector(".bottom").innerText = shape;
 
-document.querySelector(".number").innerText = number;
-document.querySelector(".top").innerText = shape;
-document.querySelector(".bottom").innerText = shape;
+  if (shape === "♥" || shape === "♦") {
+    document.querySelector(".top").style.color = "red";
+    document.querySelector(".bottom").style.color = "red";
+  } else {
+    document.querySelector(".top").style.color = "black";
+    document.querySelector(".bottom").style.color = "black";
+  }
+}
 
-if (shape === "♥" || shape === "♦" ){
-document.querySelector(".top").style.color = "red";
-document.querySelector(".bottom").style.color = "red";
-} else {
-document.querySelector(".top").style.color = "black";
-document.querySelector(".bottom").style.color = "black";
-}
-}
-const button = document.querySelector(".button")
-if (button) {
-  button.addEventListener("click", window.onload)
-}
+document.getElementById("newCard").addEventListener("click", generateCard);
